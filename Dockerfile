c3f52e59d50c09a06e0a9b2c1dfc5ff454ca15dc
Zic <git@zicstardust.com>
2026-09-05 17:18:59 -0400

    Refactor server configuration and update scripts; remove legacy cache handling

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM debian:13.6-slim
 
+#ARG BRANCHE=public
+#ARG BRANCHE=42.19
+#ARG BRANCHE=legacy41
+
 ENV DEBIAN_FRONTEND="noninteractive"
 ENV LANG=en_US.UTF-8
 ENV LC_ALL=en_US.UTF-8
@@ -35,12 +39,15 @@ RUN chmod +x /entrypoint.sh; \
 	useradd -m -u 1000 -g 1000 -s /sbin/nologin pzserver; \
 	mkdir -p /home/pzserver; \
 	\
+	echo "${BRANCHE:-public}" > /home/pzserver/branch.txt; \
+	\
 	sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen; \
-	locale-gen
+	locale-gen; \
+	steamcmd.sh +force_install_dir /app +login anonymous +app_update 380870 validate -beta "${BRANCHE:-public}" +quit
 
 EXPOSE 16261/udp
 EXPOSE 16262/udp
-EXPOSE 27015
+#EXPOSE 27015
 
 VOLUME [ "/data" ]
 
--- src/configure_server.sh
+++ src/configure_server.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
 
+STEAM_BRANCHE=$(cat /home/pzserver/branch.txt)
+
+
+echo $STEAM_BRANCHE
+sleep 5
+
 set -e
 : "${UPDATE_JRE:=false}"
 : "${STEAM:=true}"
 : "${DISABLE_MOD_DOWNLOADER:=false}"
 
-BUILD=$1
+#Check update
+echo "Checking for updates..."
+steamcmd.sh +force_install_dir /app +login anonymous +app_update 380870 validate -beta "${STEAM_BRANCHE}" +quit
 
 #Set MAX_RAM
+echo "Setting max RAM to ${MAX_RAM:-8g}..."
 sed -i "s/Xmx8g/Xmx${MAX_RAM:-8g}/" /app/ProjectZomboid64.json
 
 #Set STEAM
+echo "Setting Steam to ${STEAM}..."
 if [[ "$STEAM" =~ ^(0|false|False|n|N)$ ]]; then
     sed -i "s/-Dzomboid.steam=1/-Dzomboid.steam=0/" /app/ProjectZomboid64.json
 fi
 
 #Update JRE
 if [[ "$UPDATE_JRE" =~ ^(1|true|True|y|Y)$ ]]; then
-     if [ "$BUILD" == "41" ]; then
+     if [ "$STEAM_BRANCHE" == "legacy41" ]; then
         JRE_MAJOR_VERSION="17"
     else
         JRE_MAJOR_VERSION="25"
@@ -26,7 +36,7 @@ if [[ "$UPDATE_JRE" =~ ^(1|true|True|y|Y)$ ]]; then
     JRE_URL=$(curl -s "https://api.azul.com/metadata/v1/zulu/packages/?java_version=${JRE_MAJOR_VERSION}&os=linux&arch=x64&archive_type=tar.gz&java_package_type=jre&availability_types=ca&crac_supported=false&javafx_bundled=false&latest=true" | jq -r '.[0].download_url')
     JRE_VERSION=$(echo "$JRE_URL" | sed -n 's/.*zulu\([0-9.]*-ca-jre[0-9.]*\).*/\1/p')
 
-    echo "Update default Java Runtime ${JRE_MAJOR_VERSION} to version ${JRE_VERSION}..."
+    echo "Update default Java Runtime ${JRE_MAJOR_VERSION}..."
     rm -Rf /app/jre64
     wget -q ${JRE_URL}
     tar -xf zulu${JRE_VERSION}-linux_x64.tar.gz
